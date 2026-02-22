de3bc4efdac7fc785da8adc4e296a9b93116a971
Aryan singh bhadoria <aryanbhadoria100@gmail.com>
2026-02-22 20:14:49 +0530

    valid 2

--- valid palindrom.cpp
+++ valid palindrom.cpp
@@ -26,3 +26,31 @@
 //         return true;
 //     }
 // };
+class Solution {
+public:
+
+    bool fun(string &s, int low, int high) {
+        if (low >= high)
+            return true;
+
+        if (s[low] != s[high])
+            return false;
+
+        return fun(s, low + 1, high - 1);
+    }
+
+    bool isPalindrome(string s) {
+        
+        // Step 1: Clean the string
+        string temp = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (isalnum(s[i])) {              // keep letters & numbers
+                temp += tolower(s[i]);        // convert to lowercase
+            }
+        }
+
+        // Step 2: Check palindrome
+        return fun(temp, 0, temp.length() - 1);
+    }
+};
