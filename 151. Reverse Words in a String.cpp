f6a2d56a5b23d1fde6dd504f478a26eea79ba3a1
Aryan singh bhadoria <aryanbhadoria100@gmail.com>
2026-03-10 17:55:36 +0530

    151. Reverse Words in a String

---
+++ 151. Reverse Words in a String.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        
+        vector<string> words;
+        string temp = "";
+        
+        for(char c : s)
+        {
+            if(c != ' ')
+                temp += c;
+            else
+            {
+                if(temp != "")
+                {
+                    words.push_back(temp);
+                    temp = "";
+                }
+            }
+        }
+        
+        if(temp != "")
+            words.push_back(temp);
+        
+        reverse(words.begin(), words.end());
+        
+        string ans = "";
+        
+        for(int i = 0; i < words.size(); i++)
+        {
+            ans += words[i];
+            if(i != words.size()-1)
+                ans += " ";
+        }
+        
+        return ans;
+    }
+};
