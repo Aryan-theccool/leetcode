c5f04d3aa70fea65ed517aa4cc447b8f60f26517
Aryan-theccool <146411259+Aryan-theccool@users.noreply.github.com>
2026-04-12 17:10:43 +0530

    Implement isMonotonic function for array check
    Refactor isMonotonic function to check for increasing and decreasing order.

---
+++ Monotonic Array.cpp
@@ -0,0 +1,28 @@
+// class Solution {
+// public:
+//     bool isMonotonic(vector<int>& nums) {
+//         bool ans=false;
+//      for(int i=0;i<nums.size()-1;i++){
+//         if(nums[i]>=nums[i+1] || nums[i]=<nums[i+1]){
+//             ans= true;
+//         }else{
+//             ans=false;
+//         }
+//      }
+//      return ans;   
+//     }
+// };
+
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        bool inc = true, dec = true;
+
+        for(int i = 0; i < nums.size() - 1; i++) {
+            if(nums[i] > nums[i+1]) inc = false;
+            if(nums[i] < nums[i+1]) dec = false;
+        }
+
+        return inc || dec;
+    }
+};
